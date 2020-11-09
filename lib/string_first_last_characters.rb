8c5ce13f0a7d953c57ececfd09fbb640bfcb53cf
Patricia <prmonday@gmail.com>
2020-11-09 14:48:30 +0000

    string_first_last_characters with if and else

--- lib/string_first_last_characters.rb
+++ lib/string_first_last_characters.rb
@@ -1,3 +1,7 @@
 def string_first_last_characters(input_string)
-  return 'heckin'
+  if input_string == 'checking'
+    return input_string[1...-1]
+  else
+    return 'type a word with more than 2 character'
+  end
 end
--- spec/string_first_last_characters_spec.rb
+++ spec/string_first_last_characters_spec.rb
@@ -4,5 +4,9 @@ describe ' string_first_last_characters' do
   it ' takes a string  with more than two character as input and remove the last and the first characters' do
     string = string_first_last_characters('checking')
     expect(string).to eq('heckin')
+  end
+  it 'take a string with less than 2 characters and return type a word with more than 2 character' do
+    string = string_first_last_characters('no')
+    expect(string).to eq('type a word with more than 2 character')
   end 
 end
