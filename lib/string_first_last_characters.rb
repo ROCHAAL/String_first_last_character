6ecbc606d7a1d186f06677f1d9bdf41133658d2f
Patricia <prmonday@gmail.com>
2020-11-11 09:52:37 +0000

    trying to use length, map and slice method

--- lib/string_first_last_characters.rb
+++ lib/string_first_last_characters.rb
@@ -1,7 +1,10 @@
 def string_first_last_characters(input_string)
-  if input_string == 'checking' ||  input_string == 'big'
-    return input_string[1...-1]
+  if input_string.lenght > 2
+      input_string.map do
+      |n| n.slice[1...-1]
   else
     return 'type a word with more than 2 character'
+  end
+
   end
 end
