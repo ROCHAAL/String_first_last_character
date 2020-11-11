854bd5d810bd40a234bf86417f06e9d007711d4c
Patricia <prmonday@gmail.com>
2020-11-11 10:44:26 +0000

    else without rescue is useless error

--- lib/string_first_last_characters.rb
+++ lib/string_first_last_characters.rb
@@ -1,10 +1,9 @@
 def string_first_last_characters(input_string)
-  if input_string.lenght > 2
-      input_string.map do
-      |n| n.slice[1...-1]
-  else
-    return 'type a word with more than 2 character'
-  end
 
-  end
+    if input_string.map do
+      |n|  n.lenght > 2.slice[1...-1]
+    else
+      return 'type a word with more than 2 characters'
+    end
+    end
 end
